f9c27c83e92498145d2626383d0b55a81e503975
Derek Wright <dmwright1@paychex.com>
2015-11-10 10:01:10 -0500

    Setup guard + minitest and added some basic tests for ::Client

---
+++ test/ucsman/test_client.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class UcsmanTest < Minitest::Test
+  def setup
+    @ucs = Ucsman::Client.new(
+      {
+        username: 'admin',
+        password: '',
+        hostname: '192.168.56.103',
+        verify_ssl: false
+      }
+    )
+    @ucs.login
+  end
+
+  def teardown
+    @ucs.logout
+  end
+
+  def test_that_it_initialized
+    assert_instance_of(Ucsman::Client, @ucs)
+  end
+
+  def test_that_it_is_logged_in
+    assert(@ucs.logged_in?)
+    puts @ucs.logged_in?
+  end
+
+  def test_that_it_responds_to_macpool
+    assert_respond_to(@ucs, 'macpool')
+  end
+
+  def test_that_it_responds_to_wwnnpool
+    assert_respond_to(@ucs, 'wwnnpool')
+  end
+
+  def test_that_it_responds_to_wwpnpool
+    assert_respond_to(@ucs, 'wwpnpool')
+  end
+end
